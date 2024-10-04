619dca0b6826b08cf444c4e8e8ea55f0c6e4b534
cr7 <cr7>
2024-10-04 20:28:36 +0700

    no message

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -67,7 +67,14 @@ else:
     title = app.config['TITLE']
 
 # Redis Connection
-r = redis.Redis()
+# r = redis.Redis()
+redis_server = os.environ['REDIS']
+
+try:
+      r = redis.Redis(redis_server)
+      r.ping()
+except redis.ConnectionError:
+      exit('Failed to connect to Redis, terminating.')
 
 # Change title to host name to demo NLB
 if app.config['SHOWHOST'] == "true":
